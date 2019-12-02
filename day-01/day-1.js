c687427462ea0d72405696ee9d384a02307f6b31
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2019-12-02 21:24:30 +0100

    D1-2: With Fuel

--- day-01/day-1.js
+++ day-01/day-1.js
@@ -2,16 +2,29 @@ const fs = require('fs');
 const path = require('path');
 
 const readFile = (filePath) => fs.readFileSync(path.resolve(__dirname, filePath), 'utf-8');
-const fuelForModule = (mass) => parseInt(mass / 3) - 2;
-const fuelForMass = f => f;
+const fuelForModule = (module) => parseInt(module / 3) - 2;
+const fuelForModuleWithFuel = (module) => {
+    let fuel = fuelForModule(module);
+    if (fuel > 0) {
+        fuel += fuelForModuleWithFuel(fuel);
+    }
+    return fuel > 0 ? fuel : 0;    
+}
 
 const inputLines = readFile("./input.txt").toString().split(/\r?\n/);
-const required = inputLines
+const requiredForModule = inputLines
     .map(mass => fuelForModule(mass))
     .reduce((a, b) => a + b, 0);
-console.log(required);
+const requiredForModuleAndFuel = inputLines
+    .map(mass => {
+        let fuel = fuelForModule(mass)
+        return fuel + fuelForModuleWithFuel(fuel)
+    })
+    .reduce((a, b) => a + b, 0);
+
+console.log(requiredForModuleAndFuel);
 
 module.exports = {
     fuelForModule: fuelForModule,
-    fuelForMass: fuelForMass,
+    fuelForModuleWithFuel: fuelForModuleWithFuel,
 };
