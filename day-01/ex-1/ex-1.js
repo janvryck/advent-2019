4d252f60dff95fba28dd9c4c1aa419e694340954
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2019-12-02 18:25:29 +0100

    D1-1: Extract const

--- day-01/ex-1/ex-1.js
+++ day-01/ex-1/ex-1.js
@@ -4,7 +4,8 @@ const path = require('path');
 const readFile = (filePath) => fs.readFileSync(path.resolve(__dirname, filePath), 'utf-8');
 const fuelForMass = (mass) => parseInt(mass / 3) - 2;
 
-const required = readFile("./input.txt").toString().split(/\r?\n/)
+const inputLines = readFile("./input.txt").toString().split(/\r?\n/);
+const required = inputLines
     .map(mass => fuelForMass(mass))
     .reduce((a, b) => a + b, 0);
 console.log(required);
