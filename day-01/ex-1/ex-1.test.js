66d23356a97248d18182c32639bf38c36a8b3bec
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2019-12-02 18:03:14 +0100

    D1-1: Solve

--- day-01/ex-1/ex-1.test.js
+++ day-01/ex-1/ex-1.test.js
@@ -1,8 +1,8 @@
 const fuelForMass = require('./ex-1')
 
-test("Calculates correct fuel requirements for mass", () => {
-    expect(fuelForMass(12)).toBe(2);
-    expect(fuelForMass(14)).toBe(2);
-    expect(fuelForMass(1969)).toBe(654);
-    expect(fuelForMass(100756)).toBe(33583);
+describe("Calculates correct fuel requirements for mass", () => {
+    it("Mass 12 yields 2 fuel", () => { expect(fuelForMass(12)).toBe(2); });
+    it("Mass 14 yields 2 fuel", () => { expect(fuelForMass(14)).toBe(2); });
+    it("Mass 1969 yields 654 fuel", () => { expect(fuelForMass(1969)).toBe(654); });
+    it("Mass 100756 yields 33583 fuel", () => { expect(fuelForMass(100756)).toBe(33583); });
 })
