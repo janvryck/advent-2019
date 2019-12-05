6f51693b69c2649e22e7ab6709372ffd8db6da87
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2019-12-05 09:06:55 +0100

    D2-1: Impl

--- day-02/day-2.js
+++ day-02/day-2.js
@@ -1,6 +1,50 @@
-const executeIntCode = (intCodeInput) => {
+const fs = require('fs');
+const path = require('path');
 
+const readFile = (filePath) => fs.readFileSync(path.resolve(__dirname, filePath), 'utf-8');
+
+const getOperand = function (prog, currentPos, idx) {
+    const operandPosition = prog[currentPos + idx];
+    return prog[operandPosition];
 };
+
+const calculateOutcome = function (prog, position) {
+    const firstOperand = getOperand(prog, position, 1);
+    const secondOperand = getOperand(prog, position, 2);
+    return prog[position] === 1
+        ? firstOperand + secondOperand
+        : firstOperand * secondOperand;
+};
+
+const executeCurrentOperation = function (prog, position) {
+    const outcome = calculateOutcome(prog, position);
+    const outcomeIdx = prog[position + 3];
+    prog[outcomeIdx] = outcome;
+};
+
+const executeIntCode = (intCodeInput) => {
+    if (!Array.isArray(intCodeInput) || intCodeInput <= 0) {
+        throw new Error("Input should be a non-empty array.")
+    }
+    const prog = intCodeInput;
+
+    let position = 0;
+    while (prog[position] !== 99) {
+        executeCurrentOperation(prog, position);
+        position += 4;
+    }
+
+    return prog;
+};
+
+const inputProg = () => {
+    const rawInput = readFile("./input.txt").toString();
+    const prog1202 = rawInput.split(',');
+    prog1202[1] = 12;
+    prog1202[2] = 2;
+    return prog1202.map(i => Number(i));
+};
+console.log(executeIntCode(inputProg())[0]);
 
 module.exports = {
     executeIntCode: executeIntCode
--- day-02/day-2.test.js
+++ day-02/day-2.test.js
@@ -1,29 +1,39 @@
 const {executeIntCode} = require('./day-2');
 
 describe("Executes IntCode programs correctly", () => {
-    it("Executes the first sample correctly", () => { 
-        const given = "1,9,10,3,2,3,11,0,99,30,40,50";
-        const expected = "3500,9,10,70,2,3,11,0,99,30,40,50";
-        expect(executeIntCode(given)).toEqual(expected); 
+    it("Expects an array as input", () => {
+        expect(() => executeIntCode(undefined)).toThrowError(new Error("Input should be a non-empty array."));
+        expect(() => executeIntCode(0)).toThrowError(new Error("Input should be a non-empty array."));
+        expect(() => executeIntCode("string")).toThrowError(new Error("Input should be a non-empty array."));
+        expect(() => executeIntCode([])).toThrowError(new Error("Input should be a non-empty array."));
+        expect(() => executeIntCode([99])).not.toThrowError(new Error("Input should be a non-empty array."));
+    })
+});
+
+describe("Executes IntCode programs correctly", () => {
+    it("Executes the first sample correctly", () => {
+        const given = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+        const expected = [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50];
+        expect(executeIntCode(given)).toEqual(expected);
     });
-    it("Executes the second sample correctly", () => { 
-        const given = "1,9,10,3,2,3,11,0,99,30,40,50";
-        const expected = "3500,9,10,70,2,3,11,0,99,30,40,50";
-        expect(executeIntCode(given)).toEqual(expected); 
+    it("Executes the second sample correctly", () => {
+        const given = [1, 0, 0, 0, 99];
+        const expected = [2, 0, 0, 0, 99];
+        expect(executeIntCode(given)).toEqual(expected);
     });
-    it("Executes the third sample correctly", () => { 
-        const given = "1,9,10,3,2,3,11,0,99,30,40,50";
-        const expected = "3500,9,10,70,2,3,11,0,99,30,40,50";
-        expect(executeIntCode(given)).toEqual(expected); 
+    it("Executes the third sample correctly", () => {
+        const given = [2, 3, 0, 3, 99];
+        const expected = [2, 3, 0, 6, 99];
+        expect(executeIntCode(given)).toEqual(expected);
     });
-    it("Executes the fourth sample correctly", () => { 
-        const given = "1,9,10,3,2,3,11,0,99,30,40,50";
-        const expected = "3500,9,10,70,2,3,11,0,99,30,40,50";
-        expect(executeIntCode(given)).toEqual(expected); 
+    it("Executes the fourth sample correctly", () => {
+        const given = [2, 4, 4, 5, 99, 0];
+        const expected = [2, 4, 4, 5, 99, 9801];
+        expect(executeIntCode(given)).toEqual(expected);
     });
-    it("Executes the fifth sample correctly", () => { 
-        const given = "1,9,10,3,2,3,11,0,99,30,40,50";
-        const expected = "3500,9,10,70,2,3,11,0,99,30,40,50";
-        expect(executeIntCode(given)).toEqual(expected); 
+    it("Executes the fifth sample correctly", () => {
+        const given = [1, 1, 1, 4, 99, 5, 6, 0, 99];
+        const expected = [30, 1, 1, 4, 2, 5, 6, 0, 99];
+        expect(executeIntCode(given)).toEqual(expected);
     });
 });
