833fc7a1544ba400529f82ce42f753521cac195c
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2019-12-07 18:50:48 +0100

    D5-1: Impl

---
+++ day-05/day-5.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const path = require('path');
+
+let memory, input, output;
+
+const readFile = (filePath) => fs.readFileSync(path.resolve(__dirname, filePath), 'utf-8');
+
+const init = function() {
+    memory = [];
+    input = undefined;
+    output = [];
+};
+
+const paramsFrom = function (instruction, position) {
+    const paramModes = Math.floor(instruction / 100).toString()
+        .split('')
+        .reverse();
+    const params = [];
+    for (let i = 1; i <= 3; i++) {
+        let param = memory[position + i];
+        params.push(paramModes[i - 1] === "1" ? param : memory[param])
+    }
+    return params;
+};
+
+const decodeInstruction = function (position) {
+    let instruction = memory[position];
+    const params = paramsFrom(instruction, position);
+
+    switch (instruction % 100) {
+        case 1:
+            // add
+            memory[memory[position + 3]] = params[0] + params[1];
+            return position + 4;
+        case 2:
+            // multiply
+            memory[memory[position + 3]] = params[0] * params[1];
+            return position + 4;
+        case 3:
+            // store
+            memory[memory[position + 1]] = input;
+            return position + 2;
+        case 4:
+            // retrieve
+            output.push(memory[memory[position + 1]]);
+            return position + 2;
+        case 99:
+            // halt
+            return -1;
+        default:
+            throw new Error("Unknown instruction: " + instruction);
+    }
+};
+
+const runProgram = function (program, i = 1) {
+    memory = program.split(',').map(m => Number(m));
+    input = i;
+    let position = 0;
+    while (position >= 0) {
+        position = decodeInstruction(position);
+    }
+    console.log(output.reverse()[0]);
+};
+
+const part1 = function () {
+    init();
+    runProgram(readFile('input.txt'));
+}();
