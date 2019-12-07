271261245bc8eb52e34af0cfa4363639758b0cf5
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2019-12-07 13:06:05 +0100

    D4-1: Impl

---
+++ day-04/day-4.js
@@ -0,0 +1,55 @@
+const input = [265275, 781584];
+
+const generateRange = function (lower, upper) {
+    const potentialPasswords = [];
+    for (let i = lower; i <= upper; i++) {
+        potentialPasswords.push(i.toString());
+    }
+    return potentialPasswords;
+};
+
+const isPotential = function (p) {
+    return isOrdered(p) && containsDuplicates(p)
+};
+
+const isOrdered = function (p) {
+    return strictEqual(p.split(''), p.split('').sort());
+};
+
+const strictEqual = function (a, b) {
+    if (a === b) {
+        return true;
+    }
+    if (a == null || b == null) {
+        return false;
+    }
+    if (a.length !== b.length) {
+        return false;
+    }
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+};
+
+const containsDuplicates = function (o) {
+    for (const c of o) {
+        let occurences = (o.match(new RegExp(c, "g")) || []).length;
+        if (occurences > 1) {
+            return true
+        }
+    }
+    return false;
+};
+
+const part1 = function () {
+    return generateRange(input[0], input[1])
+        .filter(isPotential)
+        .length;
+};
+
+console.log(part1());
+
+module.exports = {
+    isPotential: isPotential
+};
