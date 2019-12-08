7b0d6bb02e979efced5541eae71556e7ce557f9e
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2019-12-08 11:12:22 +0100

    D6-1: Impl

--- day-06/day-06.js
+++ day-06/day-06.js
@@ -1,4 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+
+const readFile = (filePath) => fs.readFileSync(path.resolve(__dirname, filePath), 'utf-8');
+
+const totalOrbitsFor = function (input) {
+    const orbits = buildOrbitsFrom(input);
+
+    return Object.keys(orbits)
+        .map(k => orbitsFor(orbits, k))
+        .reduce((a, b) => Number(a) + Number(b));
+};
+
+const buildOrbitsFrom = function (input) {
+    const orbits = {};
+    input.forEach(orbit => addOrbit(orbit, orbits));
+    return orbits
+};
+
+function addOrbit(orbit, orbits) {
+    let [mass, orbiter] = orbit.split(")");
+    if (orbits[mass] !== undefined) {
+        orbits[mass].push(orbiter);
+    } else {
+        orbits[mass] = [orbiter];
+    }
+}
+
+function orbitsFor(orbits, key) {
+    let amount = 0;
+    if (orbits[key] !== undefined) {
+        amount += orbits[key].length;
+        amount += orbits[key].map(suborbit => orbitsFor(orbits, suborbit))
+            .reduce((a, b) => a + b);
+    }
+    return Number(amount);
+}
+
+const part1 = function () {
+    const inputLines = readFile("./input.txt").toString().split(/\r?\n/);
+    return totalOrbitsFor(inputLines);
+};
+
+console.log(part1());
 
 module.exports = {
-    totalOrbitsFor: () => 0
+    totalOrbitsFor: totalOrbitsFor
 };
--- day-06/day-06.test.js
+++ day-06/day-06.test.js
@@ -2,7 +2,7 @@ const {totalOrbitsFor} = require('./day-06');
 
 describe("Calculating the total amount of direct and indirect orbits", () => {
     it("Correctly calculates the first sample", () => {
-        const input = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L";
+        const input = ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"];
 
         expect(totalOrbitsFor(input)).toBe(42);
     })
