514f31c5b172e6f46097c9889b24c736098974f8
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2019-12-09 13:53:20 +0100

    D6-2: Impl

--- day-06/day-06.js
+++ day-06/day-06.js
@@ -36,15 +36,43 @@ function orbitsFor(orbits, key) {
     return Number(amount);
 }
 
-const transfersBetween = function(input, from, to) {
+const transfersBetween = function (input, from, to) {
+    const orbits = buildOrbitsFrom(input);
 
+    const ancestorsFrom = findAncestors(orbits, from);
+    const ancestorsTo = findUntilCommonAncestor(orbits, to, ancestorsFrom);
+
+    const commonAncestor = ancestorsTo[ancestorsTo.length -1];
+
+    return ancestorsFrom.indexOf(commonAncestor) + ancestorsTo.indexOf(commonAncestor);
+};
+
+const findAncestors = function(orbits, child) {
+    return findUntilCommonAncestor(orbits, child, []);
+};
+
+const findUntilCommonAncestor = function(orbits, child, known) {
+    let ancestors = [];
+    let parent = child;
+    do {
+        parent = Object.keys(orbits)
+            .find(k => orbits[k].includes(parent));
+        if (parent !== undefined) ancestors.push(parent);
+    } while(parent !== undefined && !known.includes(parent));
+    return ancestors;
 };
 
 const part1 = function () {
     const inputLines = readFile("./input.txt").toString().split(/\r?\n/);
     return totalOrbitsFor(inputLines);
 };
-// console.log(part1());
+console.log(part1());
+
+const part2 = function () {
+    const inputLines = readFile("./input.txt").toString().split(/\r?\n/);
+    return transfersBetween(inputLines, "YOU", "SAN");
+};
+console.log(part2());
 
 module.exports = {
     totalOrbitsFor: totalOrbitsFor,
