bd91d92e2abc0f710b67cc265de00fb3471f8e3e
Jan Van Ryckeghem <jan.vanryckeghem@gmail.com>
2019-12-12 08:26:00 +0100

    D7-1: Impl

--- day-07/day-7.js
+++ day-07/day-7.js
@@ -1,3 +1,39 @@
+const {readFile} = require("../lib/read-file");
+const {intCode} = require("../lib/int-code");
+
+const maxThrusterSettingFor = function (program, ...phase) {
+    let output = 0;
+    for (let p of phase) {
+        output = intCode(program, ...[p, output])[0];
+    }
+    return output;
+};
+
+const generateCombinations = function () {
+    const combinations = [];
+    for (let setting = 0; setting < Math.pow(5, 5); setting++) {
+        combinations.push(setting.toString(5).padStart(5, '0')
+            .split('')
+            .map(x => Number(x)))
+    }
+    return combinations.filter(c => (new Set(c)).size === c.length);
+};
+
+const maxForCombinations = function (program) {
+    const settings = generateCombinations();
+    let max = Number.MIN_VALUE;
+    for (let s of settings) {
+        const maxThruster = maxThrusterSettingFor(program, ...s);
+        max = Math.max(max, maxThruster);
+    }
+    return max;
+};
+
+const part1 = function () {
+    let program = readFile('../day-07/input.txt');
+    console.log(maxForCombinations(program));
+}();
+
 module.exports = {
-    maxThrusterSetting: () => undefined
+    maxThrusterSetting: maxThrusterSettingFor
 };
